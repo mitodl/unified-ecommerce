eddb7cc1e0b756cdb6a5e7af0438ae81cf135fc8
James Kachel <jkachel@mit.edu>
2024-02-28 15:00:21 -0600

    No-verify commit to get the test endpoints in place
    (these will likely need some help before they're good)

--- system_meta/views.py
+++ system_meta/views.py
@@ -1,13 +1,36 @@
 """Views for the REST API for system metadata."""
 
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+import base64
+import json
+import logging
+
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+from rest_framework.permissions import (
+    IsAuthenticated,
+)
+from rest_framework.response import Response
 
 from system_meta.models import IntegratedSystem, Product
 from system_meta.serializers import IntegratedSystemSerializer, ProductSerializer
 
 
 class IntegratedSystemViewSet(viewsets.ModelViewSet):
+from unified_ecommerce.authentication import (
+    ApiGatewayAuthentication,
+)
+from unified_ecommerce.permissions import (
+    IsAdminUserOrReadOnly,
+)
+from unified_ecommerce.viewsets import AuthVariegatedModelViewSet
+
+log = logging.getLogger(__name__)
+
     """Viewset for IntegratedSystem model."""
 
     queryset = IntegratedSystem.objects.all()
@@ -21,3 +44,58 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAuthenticated,)
+@api_view(["GET"])
+@authentication_classes(
+    [
+        ApiGatewayAuthentication,
+    ]
+)
+@permission_classes([])
+def apisix_test_request(request):
+    """Test API request so we can see how the APISIX integration works."""
+
+    def decode_x_header(header):
+        x_userinfo = request.META.get(header, False)
+
+        if not x_userinfo:
+            return f"No {header} header"
+
+        decoded_x_userinfo = base64.b64decode(x_userinfo)
+        return json.loads(decoded_x_userinfo)
+
+    response = {
+        "name": "Response ok!",
+        "user": request.user.username if request.user is not None else None,
+        "x_userinfo": decode_x_header("HTTP_X_USERINFO"),
+        "x_id_token": decode_x_header("HTTP_X_ID_TOKEN"),
+    }
+
+    return Response(response, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+@permission_classes([])
+def traefik_test_request(request):
+    """Test API request so we can see how the Traefik integration works."""
+
+    response = {
+        "name": "Response ok!",
+        "user": request.user.username if request.user is not None else None,
+        "metas": [f"{key}: {value}" for key, value in request.META.items()],
+    }
+
+    return Response(response, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def authed_traefik_test_request(request):
+    """Test API request so we can see how the Traefik integration works."""
+
+    response = {
+        "name": "Response ok!",
+        "user": request.user.username if request.user is not None else None,
+        "metas": [f"{key}: {value}" for key, value in request.META.items()],
+    }
+
+    return Response(response, status=status.HTTP_200_OK)
