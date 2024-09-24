5b465a1cdf284e961c1a1f1dd437a0b95d14f329
CP <collinp@mit.edu>
2024-09-24 09:22:34 -0400

    Add migrations to release script

--- scripts/heroku-release-phase.sh
+++ scripts/heroku-release-phase.sh
@@ -10,6 +10,12 @@ MANAGE_FILE=$(find . -maxdepth 3 -type f -name 'manage.py' | head -1)
 # trim "./" from the path
 MANAGE_FILE=${MANAGE_FILE:2}
 
+# Run migrations
+
+echo "-----> Running django migrations"
+python $MANAGE_FILE showmigrations --list 2>&1 | indent
+python $MANAGE_FILE migrate --noinput 2>&1 | indent
+
 # Retaining this script in the should we require future post release actions
 # we will have a recipe set to go.
 echo "----> NOOP"
