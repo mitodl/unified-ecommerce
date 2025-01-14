8fd1c2ad25f4690fbd3b9064e2741adfde68dcaa
James Kachel <jkachel@mit.edu>
2025-01-14 14:17:33 -0600

    adding tests for basket with product
    this includes just base tests for normal operation, which didn't exist

--- payments/views/v0/v0_test.py
+++ payments/views/v0/v0_test.py
@@ -1,6 +1,7 @@
 """View tests for the v0 API."""
 
 import pytest
+from django.urls import reverse
 
 from payments.factories import BasketFactory, DiscountFactory, ProductFactory
 from payments.models import Basket
@@ -29,7 +30,7 @@ def test_create_basket_with_products(
         BasketFactory(user=user, integrated_system=system) if existing_basket else None
     )
 
-    url = "/api/v0/payments/baskets/create_with_products/"
+    url = reverse("v0:create_with_products")
     payload = {
         "system_slug": system.slug,
         "skus": [{"sku": product.sku, "quantity": 1} for product in products],
@@ -58,3 +59,76 @@ def test_create_basket_with_products(
 
     if add_discount:
         assert discount in Basket.objects.get(id=basket_id).discounts.all()
+
+
+@pytest.mark.parametrize(
+    ("existing_basket", "add_discount", "bad_product", "bad_discount"),
+    [
+        (True, False, False, False),
+        (False, True, False, False),
+        (False, False, True, False),
+        (False, True, False, True),
+    ],
+)
+def test_create_basket_with_product(
+    mocker, user, user_client, existing_basket, add_discount, bad_product, bad_discount
+):
+    """Test creating a basket with a single product, and/or a discount."""
+
+    mocker.patch("payments.api.send_pre_sale_webhook")
+    system = ActiveIntegratedSystemFactory()
+
+    if bad_product:
+        product = ProductFactory.create()
+    else:
+        product = ProductFactory.create(system=system)
+
+    basket = (
+        BasketFactory(user=user, integrated_system=system) if existing_basket else None
+    )
+
+    url = reverse(
+        "v0:create_from_product",
+        kwargs={"system_slug": system.slug, "sku": product.sku},
+    )
+
+    if add_discount:
+        if bad_discount:
+            discount = DiscountFactory(
+                discount_type="fixed-price",
+                amount=100,
+                integrated_system=ActiveIntegratedSystemFactory(),
+            )
+        else:
+            discount = DiscountFactory(discount_type="fixed-price", amount=100)
+
+        url = reverse(
+            "v0:create_from_product_with_discount",
+            kwargs={
+                "system_slug": system.slug,
+                "sku": product.sku,
+                "discount_code": discount.discount_code,
+            },
+        )
+
+    response = user_client.post(url)
+
+    if bad_product:
+        assert response.status_code == 404
+        return
+
+    # This returns a 201 if we created the _basket line item_.
+    assert response.status_code >= 200
+    assert response.status_code < 300
+
+    basket_id = response.data["id"]
+    assert Basket.objects.get(id=basket_id).basket_items.count() == 1
+
+    if existing_basket:
+        assert basket_id == basket.id
+
+    if add_discount:
+        if bad_discount:
+            assert discount not in Basket.objects.get(id=basket_id).discounts.all()
+        else:
+            assert discount in Basket.objects.get(id=basket_id).discounts.all()
